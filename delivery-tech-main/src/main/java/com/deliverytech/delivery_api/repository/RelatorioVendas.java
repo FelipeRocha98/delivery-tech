15236bf1e61f048821bc851de882ece715dee4b0
FelipeRocha98 <feliperocha0809@hotmail.com>
2025-08-15 20:38:45 -0300

    Atualizar dados do projeto

--- delivery-tech-main/src/main/java/com/deliverytech/delivery_api/repository/RelatorioVendas.java
+++
@@ -1,9 +0,0 @@
-package com.deliverytech.delivery_api.repository;
-
-import java.math.BigDecimal;
-
-public interface RelatorioVendas {
-    String getNomeRestaurante();
-    BigDecimal getTotalVendas();
-    Long getQuantidadePedidos();
-}
--- delivery-tech-main/src/main/java/com/deliverytech/delivery_api/service/PagamentoService.java
+++
@@ -1,6 +0,0 @@
-package com.deliverytech.delivery_api.service;
-
-public interface PagamentoService {
-
-    public void processPayment(Long orderId);
-}
